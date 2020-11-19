252dae0227ffedd999bd3217da85962fa49df41d
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-11-18 22:38:57 -0300

    Este programa insere na tela os números de 0 a 50

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Programa_que_escreve_50_numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escreva um algoritmo que exiba todos os números inteiros de 0 a 50.
+            for(int conta=0; conta<=50; conta++){
+                Console.WriteLine(conta);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
